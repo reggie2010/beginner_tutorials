c55d08a22dc031891b64dfe9b4c176f4e6a45e35
Vontrelle Collins <trelle823@msn.com>
2017-10-28 17:57:48 -0400

    Corrected argc

--- src/listener.cpp
+++ src/listener.cpp
@@ -5,7 +5,7 @@ void chatterCallback(const std_msgs::String::ConstPtr &msg) {
 	ROS_INFO("I heard: [%s]", msg->data.c_str());
 }
 
-int main(int agrc, char **argv) {
+int main(int argc, char **argv) {
 
 	ros::init(argc, argv, "listener");
 	ros::NodeHandle n;
