942a316793a3610b9fb5536c3ac10d2e2485772f
Vontrelle Collins <trelle823@msn.com>
2017-11-07 21:55:11 -0500

    Wrote service code to change the freqency rate of talker node

---
+++ src/changeFrequencyServer.cpp
@@ -0,0 +1,17 @@
+#include "ros/ros.h"
+#include "beginner_tutorials/changeFrequency.h"
+
+bool change(beginner_tutorials::changeFrequency::Request &req,
+            beginner_tutorials::changeFrequency::Response &res) {
+  res.frequencyOut = req.frequencyIn;
+  return true;
+}
+
+int main(int argc, char **argv) {
+  ros::init(argc, argv, "frequency_server");
+  ros::NodeHandle n;
+  ros::ServiceServer server = n.advertiseService("change_frequency", change);
+  ros::spin();
+
+  return 0;
+}
